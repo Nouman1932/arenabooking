4b7875b1bf4696e4c6e270dc769304c05f7a2764
Nouman1932 <noumanmehmood25@gmail.com>
2022-07-06 16:37:28 +0500

    Updates

---
+++ precache-manifest.ac25423fbf218ec397d0c0c1ebc83f54.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dbd3e0be1ce9902c1e24e205aab6e5",
+    "url": "/arena-booking/index.html"
+  },
+  {
+    "revision": "5f65404ff70f5bf808c1",
+    "url": "/arena-booking/static/css/2.fc679e12.chunk.css"
+  },
+  {
+    "revision": "3918612b78a3dedc41d4",
+    "url": "/arena-booking/static/css/main.d10a64a2.chunk.css"
+  },
+  {
+    "revision": "5f65404ff70f5bf808c1",
+    "url": "/arena-booking/static/js/2.d6f10da8.chunk.js"
+  },
+  {
+    "revision": "b963e22854f9fb19d1130ac43727c8ed",
+    "url": "/arena-booking/static/js/2.d6f10da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3918612b78a3dedc41d4",
+    "url": "/arena-booking/static/js/main.93e0288f.chunk.js"
+  },
+  {
+    "revision": "04ab78a3c2998c91380d",
+    "url": "/arena-booking/static/js/runtime-main.002bf34f.js"
+  },
+  {
+    "revision": "c6ed81dd7b049ea2040f1300cb78060a",
+    "url": "/arena-booking/static/media/about.c6ed81dd.svg"
+  },
+  {
+    "revision": "87e06ed694a8d72534105f7be819ff79",
+    "url": "/arena-booking/static/media/defaultBcg.87e06ed6.jpg"
+  },
+  {
+    "revision": "6edb36f3feccea6f8483d2808828b6e1",
+    "url": "/arena-booking/static/media/error.6edb36f3.jpg"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/arena-booking/static/media/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/arena-booking/static/media/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/arena-booking/static/media/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/arena-booking/static/media/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/arena-booking/static/media/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/arena-booking/static/media/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/arena-booking/static/media/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/arena-booking/static/media/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/arena-booking/static/media/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/arena-booking/static/media/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/arena-booking/static/media/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/arena-booking/static/media/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/arena-booking/static/media/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/arena-booking/static/media/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/arena-booking/static/media/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "773f054def7b6c314b5ec526b51b7e3b",
+    "url": "/arena-booking/static/media/img10.773f054d.jpeg"
+  },
+  {
+    "revision": "b2bd878663dbc84ffa2d6a92f60c15c9",
+    "url": "/arena-booking/static/media/img11.b2bd8786.jpeg"
+  },
+  {
+    "revision": "42b255125430789b6df9a94b5d9fa959",
+    "url": "/arena-booking/static/media/img2.42b25512.jpg"
+  },
+  {
+    "revision": "efa8d6e386c45782d1dd40ccc12947a6",
+    "url": "/arena-booking/static/media/img2.efa8d6e3.jpeg"
+  },
+  {
+    "revision": "a0571d4a682d015a7fb58f83e43beca7",
+    "url": "/arena-booking/static/media/img3.a0571d4a.jpeg"
+  },
+  {
+    "revision": "f9873a13e25f4be8a46748ae730c644e",
+    "url": "/arena-booking/static/media/img4.f9873a13.jpeg"
+  },
+  {
+    "revision": "9fbcea9db18c2a3638fe12d318749e37",
+    "url": "/arena-booking/static/media/img5.9fbcea9d.jpeg"
+  },
+  {
+    "revision": "3c8336b31fad30c4fc872ea86bbc4727",
+    "url": "/arena-booking/static/media/img7.3c8336b3.jpeg"
+  },
+  {
+    "revision": "2d16cfea40a3f5ba7f2f46bd16dd1995",
+    "url": "/arena-booking/static/media/img8.2d16cfea.jpeg"
+  },
+  {
+    "revision": "dcb7ab6d03c4363b96b71935c8362e50",
+    "url": "/arena-booking/static/media/img9.dcb7ab6d.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/arena-booking/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "8c3d2a79d2974762d90310ec87bec6e6",
+    "url": "/arena-booking/static/media/notfound.8c3d2a79.svg"
+  },
+  {
+    "revision": "0cd00b1b2ca2b8fc295cb41270491544",
+    "url": "/arena-booking/static/media/pool1.0cd00b1b.jpeg"
+  },
+  {
+    "revision": "30e4bf7b6e8e52bcc3a3632eb68a3030",
+    "url": "/arena-booking/static/media/pool2.30e4bf7b.jpeg"
+  },
+  {
+    "revision": "a86b0710f7ff41d285b1f71d5400548a",
+    "url": "/arena-booking/static/media/pool3.a86b0710.jpeg"
+  },
+  {
+    "revision": "2dbc4e4c3f8a48a7259c0f6343e471ab",
+    "url": "/arena-booking/static/media/pool4.2dbc4e4c.jpeg"
+  },
+  {
+    "revision": "7dd7b2ee893c1c632d16a725e565fbd6",
+    "url": "/arena-booking/static/media/stock1.7dd7b2ee.jpeg"
+  },
+  {
+    "revision": "f4e990f640c5459c7ba6918eb3bfc991",
+    "url": "/arena-booking/static/media/stock2.f4e990f6.jpeg"
+  },
+  {
+    "revision": "79c1eeadfcc737c253b17fc0c2a94420",
+    "url": "/arena-booking/static/media/stock3.79c1eead.jpeg"
+  }
+]);
